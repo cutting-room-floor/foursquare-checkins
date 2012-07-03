a8f7a0e97e8eb88b278933ddce0f1e105b797d95
Tom MacWright <tom@macwright.org>
2012-07-03 12:42:13 -0400

    First site commit

---
+++ site.js
@@ -0,0 +1,39 @@
+function $(e) {
+  return document.getElementById(e);
+}
+
+window.onload = function() {
+  wax.tilejson('http://a.tiles.mapbox.com/v3/-tmcw.foursquare-density.jsonp', function(four_density) {
+    wax.tilejson('http://a.tiles.mapbox.com/v3/tmcw.mapbox-streets-density.jsonp', function(streets_density) {
+      map = new MM.Map('map', [
+          (street_layer = new wax.mm.connector(streets_density)),
+          (four_layer = new wax.mm.connector(four_density))
+      ]);
+      map.setZoomRange(3, 9).zoom(5).center({ lat: 40, lon: 20 });
+      $('foursquare').onclick = function() {
+        this.parentNode.className = this.id;
+        street_layer.parent.style.opacity = 0;
+        four_layer.parent.style.opacity = 1;
+      };
+      $('collapse').onclick = function() {
+          if ($('block').className === 'collapsed') {
+              $('block').className = '';
+              this.innerHTML = '&#x2715;';
+          } else {
+              $('block').className = 'collapsed';
+              this.innerHTML = '+';
+          }
+      };
+      $('streets').onclick = function() {
+        this.parentNode.className = this.id;
+        street_layer.parent.style.opacity = 1;
+        four_layer.parent.style.opacity = 0;
+      };
+      $('plus').onclick = function() {
+        this.parentNode.className = this.id;
+        street_layer.parent.style.opacity = 1;
+        four_layer.parent.style.opacity = 1;
+      };
+    });
+  });
+};
